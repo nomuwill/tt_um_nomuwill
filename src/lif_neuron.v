7daf0b7fe07bf55e139bada1ff3e69619195996d
Noah Williams <nomuwill@ucsc.edu>
2024-10-28 08:28:52 -0700

    debugging

--- src/lif_neuron.v
+++ src/lif_neuron.v
@@ -1,49 +1,41 @@
-
-
 `default_nettype none
 
-module test;
+module lif (
+
+    // Define inputs 
+    input wire [7:0]    current,
+    input wire          clk,
+    input wire          reset_n,
+
+    // Define outputs
+    output reg [7:0]    state,
+    output wire         spike
+);
+
+    // Internal components
+    wire [7:0] next_state;
+    reg [7:0] threshold;
+
+    // Flipflop
+    always @(posedge clk) begin
+
+        // If reset_n is low, reset the state and threshold
+        if (!reset_n) begin
+            state <= 0;
+            threshold <= 200;
+
+        // Otherwise, update the state
+        end else begin
+            state <= next_state;
+        end
+    end
+
+    // Next state logic
+        // Shifting to right is the same as dividing by 2
+    assign next_state = current + (spike ? 0 : (state >> 1));
+
+    // Spiking logic
+    assign spike = (state >= threshold);
+
+
 endmodule
-
-
-// 'default_nettype none
-
-// module lif (
-
-//     // Define inputs 
-//     input wire [7:0]    current,
-//     input wire          clk,
-//     input wire          reset_n,
-
-//     // Define outputs
-//     output reg [7:0]    state,
-//     output wire         spike
-// );
-
-//     // Internal components
-//     wire [7:0] next_state;
-//     reg [7:0] threshold;
-
-//     // // Flipflop
-//     // always @(posedge clk) begin
-
-//     //     // If reset_n is low, reset the state and threshold
-//     //     if (!reset_n) begin
-//     //         state <= 0;
-//     //         threshold <= 200;
-
-//     //     // Otherwise, update the state
-//     //     end else begin
-//     //         state <= next_state;
-//     //     end
-//     // end
-
-//     // // Next state logic
-//     //     // Shifting to right is the same as dividing by 2
-//     // assign next_state = current + (spike ? 0 : (state >> 1));
-
-//     // // Spiking logic
-//     // assign spike = (state >= threshold);
-
-
-// endmodule
