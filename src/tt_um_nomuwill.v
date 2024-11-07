561e0af6cdfa0a9b770a49092245d69b65bfee4f
Noah Williams <nomuwill@ucsc.edu>
2024-11-07 08:31:17 -0800

    updated docs

--- src/tt_um_nomuwill.v
+++ src/tt_um_nomuwill.v
@@ -9,7 +9,7 @@ module tt_um_nomuwill (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] u`io_out,  // IOs: Output path
+    output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
@@ -18,13 +18,13 @@ module tt_um_nomuwill (
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uio_out [6:0] = 0;
-  assign uio_oe  = 1;
+  assign uio_oe  = 8'b10000000;;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, uio_in, 1'b0};
 
   // Instantiate the LIF module
-  lif izh (
+  izh izh_1(
     .current(ui_in),  // current input from parent module
     .clk(clk),        // clock driven by clock in parent module
     .reset_n(rst_n),  // reset driven by reset in parent module
