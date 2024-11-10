c9dd9a3180d12b7ea869a3754b54c197c356581a
Noah Williams <nomuwill@ucsc.edu>
2024-11-09 21:31:24 -0800

    added second neuron

--- src/tt_um_nomuwill.v
+++ src/tt_um_nomuwill.v
@@ -17,26 +17,38 @@ module tt_um_nomuwill (
 );
 
   // All output pins must be assigned. `If not used, assign to 0.
-  assign uio_out [6:0] = 0;
-  assign uio_oe = 0;
+  assign uio_out [5:0] = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, 1'b0, uio_in};
 
   // Internal signal
-  wire [7:0] v; 
-  wire spike;
+  wire [7:0] v1; 
+  wire spike1;
+  wire [7:0] v2; 
+  wire spike2;
 
-  // Instantiate the LIF module
+  // Instantiate the Izhikevich neuron
   izh izh_1(
     .current(ui_in),  // current input from parent module, concatenated to 16 bits
     .clk(clk),        // clock driven by clock in parent module
     .reset_n(rst_n),  // reset driven by reset in parent module
-    .spike(spike),    // most significant bit of state output to parent module
-    .v(v)   // Use lower 8 bits of v for state output
+    .spike(spike1),    // most significant bit of state output to parent module
+    .v(v1)   // Use lower 8 bits of v for state output
   );
 
-  assign uo_out = v; 
-  assign uio_out[7] = spike;
+  // Instantiate the Izhikevich neuron again!
+  izh izh_2(
+    .current(v1),   // Route the output of the first neuron to the second neuron
+    .clk(clk),        
+    .reset_n(rst_n),  
+    .spike(spike2),    
+    .v(v2)  
+  );
+
+  assign uo_out = v1; 
+  assign uio_oe = v2;
+  assign uio_out[7] = spike1;
+  assign uio_out[6] = spike2;
 
 endmodule
